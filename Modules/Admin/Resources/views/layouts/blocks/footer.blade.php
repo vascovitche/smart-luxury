a7d819cc170e555276147fc5c4c20b11cb6b6070
ironp <devascovitche@gmail.com>
2023-06-22 21:15:07 +0300

    fix blades

--- Modules/Admin/Resources/views/layouts/blocks/footer.blade.php
+++ Modules/Admin/Resources/views/layouts/blocks/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2022 <a href="https://github.com/vascovitche" target="_blank">Project</a>.</strong>
+    <strong>Copyright &copy; {{ now()->year }} <a href="https://github.com/vascovitche" target="_blank">Project</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 1.0
